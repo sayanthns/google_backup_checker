8f55f811fb609e2a98c980d85d71ef06dcbf9d55
sayanthns <sayanth2k16@gmail.com>
2025-05-13 23:30:52 +0530

    final

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='google_backup_checker',
     version='0.0.1',
-    description='Triggers Kuma on GDrive backup',
-    author='Your Name',
-    author_email='you@example.com',
     packages=find_packages(),
-    zip_safe=False,
     include_package_data=True,
-    install_requires=['frappe'],
+    install_requires=[],
 )
